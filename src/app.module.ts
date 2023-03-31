4d04b6a27ad60adb339e1646db25b1f5da9a6280
KimSoo <ilppang@naver.com>
2023-03-31 20:42:30 +0900

    Refactor(reviews): 파일명 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PlacesModule } from './places/places.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { ReviewModule } from './review/review.module';
+import { ReviewsModule } from './reviews/reviews.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PlacesModule, PrismaModule, ReviewModule],
+  imports: [PlacesModule, PrismaModule, ReviewsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
