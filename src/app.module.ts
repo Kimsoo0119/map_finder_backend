682de030710dbec9c3551406254f556ce007e19d
KimSoo <ilppang@naver.com>
2023-03-31 20:36:18 +0900

    Add(review): 리뷰 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PlacesModule } from './places/places.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ReviewModule } from './review/review.module';
 
 @Module({
-  imports: [PlacesModule, PrismaModule],
+  imports: [PlacesModule, PrismaModule, ReviewModule],
   controllers: [AppController],
   providers: [AppService],
 })
