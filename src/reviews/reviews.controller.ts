d91c4d778fe6652abd453dd0d87b7bedaebec371
KimSoo <ilppang@naver.com>
2023-04-14 18:50:19 +0900

    Feature(review): 간단한 리뷰 작성 기능 구현 완료 #6

--- src/reviews/reviews.controller.ts
+++ src/reviews/reviews.controller.ts
@@ -1,14 +1,30 @@
-import { Body, Controller, Get, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { ReviewsService } from './reviews.service';
 import { GetReviewsDto } from './dto/get-reviews.dto';
+import { CreateReviewDto } from './dto/create-review.dto';
 
 @Controller('reviews')
 export class ReviewsController {
   constructor(private readonly reviewsService: ReviewsService) {}
   @Get('/simple')
   async getSimpleReviews(@Query() { placeId }: GetReviewsDto) {
-    const reviews = await this.reviewsService.getSimpleReviews(placeId);
+    try {
+      const reviews = await this.reviewsService.getSimpleReviews(placeId);
 
-    return reviews;
+      return reviews;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  @Post('/simple')
+  async createSimpleReview(@Body() createReviewDto: CreateReviewDto) {
+    try {
+      await this.reviewsService.createSimpleReview(createReviewDto);
+
+      return { message: '리뷰 작성 성공' };
+    } catch (error) {
+      throw error;
+    }
   }
 }
