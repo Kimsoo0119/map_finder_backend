b8986d534e830230ec41dda85efacc45b124a9c3
kimsoo0119 <illppang@naver.com>
2023-06-25 00:01:14 +0900

    Feature(auth): 카카오톡 인가코드를 통해 access, refresh 토큰 받아오는 기능 구현 완료  #34

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Get('/signin/kakao')
+  async signinKakao(@Query('authorizationCode') authorizationCode: string) {
+    await this.authService.signinWithKakao(authorizationCode);
+  }
+}
