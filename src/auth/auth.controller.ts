9d32bc9e09b49732be62f4cba08f6548eabb9015
kimsoo0119 <illppang@naver.com>
2023-06-30 04:47:17 +0900

    Feature(auth): refresh token이 생성되면 refresh 토큰을 redis에 저장하도록 구현 완료 #41

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,10 @@
-import { CACHE_MANAGER, Controller, Get, Inject, Query } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { User } from 'src/common/interface/common-interface';
-import { Cache } from 'cache-manager';
 
 @Controller('auth')
 export class AuthController {
-  constructor(
-    @Inject(CACHE_MANAGER)
-    private cacheManager: Cache,
-    private readonly authService: AuthService,
-  ) {}
+  constructor(private readonly authService: AuthService) {}
 
   @Get('/signin/kakao')
   async signInKakao(
