c1069f8e6d33952f277e8775ae3e5c6b02c0d451
kimsoo0119 <illppang@naver.com>
2023-07-06 23:24:52 +0900

    Feature(auth): authGuard를 통해 authorization 기능 구현 완료 #36

--- src/auth/strategy/jwt.startegy.ts
+++ src/auth/strategy/jwt.startegy.ts
@@ -1,38 +1,18 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt } from 'passport-jwt';
 import { Strategy } from 'passport-jwt';
-import { AuthService } from '../auth.service';
-import { Request, Response } from 'express';
-import { PrismaService } from 'src/prisma/prisma.service';
-
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor(
-    private readonly configService: ConfigService,
-    private readonly prismaService: PrismaService,
-  ) {
+  constructor(private readonly configService: ConfigService) {
     super({
       secretOrKey: configService.get<string>('JWT_SECRET_KEY'),
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      passReqToCallback: true,
     });
   }
 
-  async validate(request: Request, tokenPayload) {
-    const { targetName, targetId } = request.body;
-    if (targetName && targetId) {
-      const user = this.prismaService[targetName].findUnique({
-        where: { id: targetId, userId: tokenPayload.userId },
-      });
-
-      if (!user) {
-        throw new UnauthorizedException('권한이 없습니다.');
-      }
-      return user;
-    }
-
-    return { userId: tokenPayload.id };
+  validate(tokenPayload) {
+    return tokenPayload;
   }
 }
--- src/common/guard/jwt-auth.guard.ts
+++ src/common/guard/jwt-auth.guard.ts
@@ -1,5 +1,55 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Users } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { TokenPayload } from '../interface/common-interface';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private readonly prisma: PrismaService) {
+    super();
+  }
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const parentCanActivate = (await super.canActivate(context)) as boolean;
+    if (!parentCanActivate) {
+      throw new ForbiddenException(`인증에 실패했습니다.`);
+    }
+
+    const request = context.switchToHttp().getRequest();
+    const tokenUser: TokenPayload = request.user;
+    const { targetName, targetId } = request.body;
+
+    const authorizedUser: Users = await this.prisma.users.findUnique({
+      where: { id: tokenUser.id },
+    });
+    if (!authorizedUser) {
+      throw new NotFoundException(`유효하지않은 유저입니다.`);
+    }
+    request.user = authorizedUser;
+
+    if (targetName && targetId) {
+      try {
+        const { userId: targetUserId } = await this.prisma[
+          targetName
+        ].findUnique({
+          where: { id: targetId },
+          select: { userId: true },
+        });
+
+        if (targetUserId !== tokenUser.id) {
+          throw new ForbiddenException(`권한이 없습니다.`);
+        }
+      } catch (error) {
+        throw new NotFoundException(`대상이 존재하지 않습니다.`);
+      }
+    }
+
+    return true;
+  }
+}
